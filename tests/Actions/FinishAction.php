aeef965b7a4bca1e3d864c156eb372f21b811b02
liuzhanpeng <liuzhanpeng@gmail.com>
2020-06-29 21:45:20 +0800

    Action的execute增加来源状态、目标状态

--- tests/Actions/FinishAction.php
+++ tests/Actions/FinishAction.php
@@ -3,10 +3,11 @@
 namespace Lzpeng\StateProcess\Tests\Actions;
 
 use Lzpeng\StateProcess\Contracts\ActionInterface;
+use Lzpeng\StateProcess\State;
 
 class FinishAction implements ActionInterface
 {
-    public function execute($domainObject)
+    public function execute($domainObject, State $fromState, State $toState)
     {
         echo 'finish...';
     }
